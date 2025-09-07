160f5d240dc21eb7947ae89bb23d183adce1baff
yehuda <yehudaperetz96@gmail.com>
2025-09-07 18:00:28 +0300

    fixed css post

--- client/src/componnet/application-layout/Post.tsx
+++ client/src/componnet/application-layout/Post.tsx
@@ -1,28 +1,27 @@
 // import fs from "fs/promises";
 
-type post = {
+export type post = {
   img: string;
   Description: string;
   author: string;
   timer: string;
-}[]
+  like : number|string
+}
 
 
-import { useState } from "react";
+import { useState, type PropsWithChildren } from "react";
 import "../../style/post.css"
-export default function Post({ post }: post) {
+export default function Post({ post }: PropsWithChildren & {post: post}) {
   const [like, setLike] = useState(0)
 
   return (
     <div className="mainPost" >
       <img className="imgPost" src={post.img} alt="" />
-      <div className="botonpPost">
-        <h3 className="TitlePost">{post.Description}</h3>
-        <button onClick={() => { setLike(like + 1) }} >{like} {post.like}</button>
-        <div className="mainTxtPost">
-          <p>author: {post.author}</p>
-          <p>{post.timer}</p>
-        </div>
+      <button onClick={() => { setLike(like + 1) }} >{like} {post.like}</button>
+      <h3 className="TitlePost">{post.Description}</h3>
+      <div className="mainTxtPost">
+        <p>author: {post.author}</p>
+        <p>{post.timer}</p>
       </div>
 
     </div>
--- client/src/componnet/application-layout/Posts.tsx
+++ client/src/componnet/application-layout/Posts.tsx
@@ -1,4 +1,4 @@
-import Post from "./Post"
+import Post, { type post } from "./Post"
 import "../../style/posts.css"
 
 export default function Posts() {
@@ -6,12 +6,12 @@ export default function Posts() {
   let time = new Date().toLocaleString()
 
 
-  let posts = [{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my post", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my post", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my post", author: "yehuda", timer: time }]
+  let posts = [{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my posts", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my post", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my post", author: "yehuda",like:"like", timer: time },{ img: "public/exsem.jpg", Description: "my post", author: "yehuda", timer: time }] as post[]
 
   return (
     <div className="mainPosts" >
-      {posts.map((e, index) => (
-        <Post index={index} post={e} />
+      {posts.map((e) => (
+        <Post  post={e} />
       ))}
     </div>
   )
